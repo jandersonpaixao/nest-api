fa4c66faa6b87a5899ceb69119054c1d776f559f
jandersonpaixao <janderson.paixao@sanar.com>
2024-06-20 16:29:01 -0300

    feat: configure docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlbumModule } from './modules/album/album.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AlbumModule],
+  imports: [
+    AlbumModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT as unknown as number,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
